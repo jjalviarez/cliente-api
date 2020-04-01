c69f37518d799f96c9c0f0c55f302f0528529cc6
Jorge Alviarez <jjafblin@gmail.com>
2020-04-01 00:20:36 -0500

    Componente Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+
+/***** Hayout */
+import Header from "./components/layout/Header";
 
 const App = () => {
   return (
-    <h1>hola</h1>
+    <Fragment> 
+      <Header />
+    </Fragment> 
     )
 }
 

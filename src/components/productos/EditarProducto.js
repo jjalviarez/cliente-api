7d424243af67fa7d75dbf675e2bbdce54e1d76bf
Jorge Alviarez <jjafblin@gmail.com>
2020-04-08 23:51:19 -0500

    Componentes de producto y productos completado

---
+++ src/components/productos/EditarProducto.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import clienteAxios from '../../config/axios';
+import Swal from 'sweetalert2';
+import { withRouter } from 'react-router-dom';
+
+const EditarProducto = props => {
+    const {id}= props.match.params
+
+    const [producto, datosProducto] = useState({
+        nombre:'',
+        precio:'',
+        imagen:''
+    });
+
+    //Query a la API Buscar uno
+    const consultaAPI = async () => {
+        const consulta = await clienteAxios.get('/productos/'+id);
+        //console.log('consulta: ', consulta.data)
+        //colocar  resultado en el state
+        datosProducto(consulta.data); 
+    }
+
+    //Query a la API Avtualizar
+    const handleSubmit =  e => {
+        e.preventDefault();
+        clienteAxios.put('/productos/'+producto._id,producto)
+            .then(res => {
+                console.log('res :', res);
+                Swal.fire(
+                    'Producto Actualizado correctamente!',
+                    'You clicked the button!',
+                    'success'
+                )
+                //redireccionar 
+                props.history.push('/productos');
+            })
+            .catch(
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                })
+            )
+        
+    }   
+    
+    
+    const handleChange = (e) => {
+        datosProducto({
+            //Actualuzamos el state agregando una copia del State actual
+            ...producto,
+            [e.target.name]:e.target.value});
+    }   
+
+
+    //user effect es similar a componetdidmont y willmount
+    useEffect(() => {
+        consultaAPI();
+        /*return () => {
+            cleanup
+        }*/
+    }, []);
+
+    return (
+        <Fragment>
+            <h2>Nuevo Producto</h2>
+            <form 
+                onSubmit={handleSubmit}
+            >
+                <legend>Llena todos los campos</legend>
+                <div className="campo">
+                    <label>Nombre:</label>
+                    <input 
+                        type="text" 
+                        placeholder="Nombre Producto" 
+                        name="nombre" 
+                        value={producto.nombre}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="campo">
+                    <label>Precio:</label>
+                    <input 
+                        type="number" 
+                        name="precio" 
+                        min={0.00} 
+                        step="0.01" 
+                        placeholder="Precio" 
+                        value={producto.precio}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="campo">
+                    <label>Imagen:</label>
+                    <input 
+                        type="file" 
+                        name="imagen" 
+                        value={producto.imagen}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="enviar">
+                    <input 
+                        type="submit" 
+                        className="btn btn-azul" 
+                        defaultValue="Agregar Producto" 
+                    />
+                </div>
+            </form>        
+        </Fragment>
+    );
+};
+
+
+
+
+export default withRouter(EditarProducto);
--- src/components/productos/Productos.js
+++ src/components/productos/Productos.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react'
+import clienteAxios from '../../config/axios';
+import { Link } from 'react-router-dom';
+import Producto from './Producto';
+
 
 const Productos = () => {
+
+    //Trabajar con el state
+    // producto = state, guardarProductos funcion para guardar el state
+    const [productos, gurardarProductos] = useState([]);
+
+    //Query a la API
+    const consultaAPI = async () => {
+        const consulta = await clienteAxios.get('/productos');
+        //colocar  resultado en el state
+        gurardarProductos(consulta.data); 
+    }
+    //user effect es similar a componetdidmont y willmount
+    useEffect(() => {
+        consultaAPI();
+        /*return () => {
+            cleanup
+        }*/
+    }, [productos]);
+
+
     return (
-        <h2>Productos</h2>
+        <Fragment> 
+            <h2>Productos</h2>
+            <Link to={"/productos/nuevo"} 
+                className="btn btn-verde nvo-cliente"> 
+                <i className="fas fa-plus-circle"></i>
+                Nuevo Producto
+            </Link>
+            <ul className="listado-productos">
+                {productos.map((producto) => (
+                    <Producto 
+                        key={producto._id}
+                        producto={producto}
+                    />
+                ))}
+            </ul>
+        </Fragment> 
     );
 };
 
